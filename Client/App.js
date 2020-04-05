565a1d51cb7e16db3f3b5529fa0a7a51dd9d2d75
leontomer <41841316+leontomer@users.noreply.github.com>
2020-04-05 18:58:51 +0300

    PM2020T20-5 redux fixed issues

--- Client/App.js
+++ Client/App.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Login from './Screens/Login/Login';
+import { createStore, combineReducers } from "redux";
+import UsersReducer from "./store/reducers/UsersReducer";
+import { Provider } from "react-redux";
+import Login from "./Screens/Login/Login";
+const rootReducer = combineReducers({
+  users: UsersReducer,
+});
+const store = createStore(rootReducer);
 
 export default function App() {
-  return <Login />;
+  return (
+    //main screen should be wrapped with provider
+    <Provider store={store}>
+      <Login />
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
