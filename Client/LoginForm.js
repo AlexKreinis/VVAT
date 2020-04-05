05d365cca6b3ba164cdbfccfbc9dc244c3bbacaf
Vl4d1s <vladismarkin@gmail.com>
2020-04-05 16:33:31 +0300

    PM2020T20-10 bulding form for the Login screen cont

--- Client/LoginForm.js
+++ Client/LoginForm.js
@@ -6,7 +6,7 @@ import React from 'react';
 
 const LoginForm = () => {
     return (
-        <View style={styles.container}>
+        <View style={formStyle.container}>
             <TextInput
                 style={formStyle.input}
                 placeholder=''
@@ -15,59 +15,24 @@ const LoginForm = () => {
                 //value={}
             />
             <TextInput
-                style={styles.input}
+                style={formStyle.input}
                 placeholder=''
                 placeholderTextColor=''
                 //onChangeText={}
                 //value={}
             />
             <TextInput
-                style={styles.input}
+                style={formStyle.input}
                 placeholder=''
                 placeholderTextColor=''
                 //onChangeText={}
                 //value={}
             />
-            <TouchableOpacity style={styles.buttonContainer}>
+            <TouchableOpacity style={formStyle.buttonContainer}>
                 <Text style="">Login</Text>
             </TouchableOpacity>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container:{
-        paddingHorizontal: 60,
-        paddingBottom: 40,
-    },
-    input:{
-        height: 40,
-        marginBottom: 10,
-        color: '#1F1F5E',
-        paddingHorizontal: 10,
-        borderRadius: 5,
-        backgroundColor: '#B9B9F3',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 3,
-        },
-        shadowOpacity: 0.27,
-        shadowRadius: 4.65,
-        elevation: 2,
-    },
-    buttonContainer:{
-        backgroundColor: "#0B0B21",
-        paddingVertical: 10,
-        marginHorizontal: 80,
-        marginVertical: 20,
-        borderRadius: 20
-    },
-    buttonText:{
-        color: '#fff',
-        textAlign: 'center',
-        fontWeight: '500'
-    }
-});
 
 export default LoginForm
