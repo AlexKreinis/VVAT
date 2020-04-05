b6e5e98e18a0bb57fd2e0519217d6167f9a69c10
leontomer <41841316+leontomer@users.noreply.github.com>
2020-04-05 16:11:45 +0300

    PM2020T20-5 redux setup

--- Client/App.js
+++ Client/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStore, combineReducers } from "redux";
+import UsersReducer from "./store/reducers/UsersReducer";
+import { Provider } from "react-redux";
+const rootReducer = combineReducers({
+  users: UsersReducer,
+});
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    //main screen should be wrapped with provider
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Client/store/reducers/UsersReducer.js
@@ -0,0 +1,12 @@
+const initialState = {};
+
+const UsersReducers = (state = initialState, action) => {
+  switch (action.type) {
+    case "":
+
+    case "  ":
+  }
+  return state;
+};
+
+export default UsersReducers;
