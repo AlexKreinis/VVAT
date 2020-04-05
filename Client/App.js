abd2a519515b351206c8d447ed6eaa9519930873
Vl4d1s <vladismarkin@gmail.com>
2020-04-05 13:22:57 +0300

    new

--- Client/App.js
+++ Client/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>New fresh project!</Text>
+      <Text>New fresh project!!!</Text>
     </View>
   );
 }
