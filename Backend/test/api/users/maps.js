474526d4018192ea15cde805385ac2b185a08f1f
vl4d1s <vladismarkin@gmail.com>
2020-04-17 17:39:57 +0300

    PM2020T-29 added map location test

---
+++ Backend/test/api/users/maps.js
@@ -0,0 +1,22 @@
+const expect = require("chai").expect;
+const request = require("supertest");
+
+const app = require("../../../server.js");
+const connectDB = require("../../../config/db");
+
+describe("Get maps", () => {
+  before((done) => {
+    connectDB();
+  });
+});
+
+it("OK. get maps", (done) => {
+  request(app)
+    .get("/api/maps/getmaps")
+    .then((res) => {
+      const body = res.body;
+      expect(body).to.contain.property("data");
+      done();
+    })
+    .catch((err) => done(err));
+});
